75d34b6c8cd905932124768da38b3958d6b2c25c
Sophie Au <SophieAu@users.noreply.github.com>
2021-07-21 23:20:13 +0200

    Tidy up playlist subscriptions

--- playlistSubscriptions.ts
+++ playlistSubscriptions.ts
@@ -5,7 +5,7 @@ import { isLessThanAWeekOld } from "./timeHelpers";
 const playlistRequestURL = (playlistID: string, queryParams: URLSearchParams) =>
   `https://api.spotify.com/v1/playlists/${playlistID}/tracks?${queryParams.toString()}`;
 
-const getTracks = async (playlistID: string) => {
+const getAllTracks = async (playlistID: string) => {
   var queryParams = new URLSearchParams();
   queryParams.append("market", "DE");
   queryParams.append("fields", "items(added_at, track.id)");
@@ -28,16 +28,24 @@ const addTracks = async (playlistID: string, trackURIs: string[]) => {
   } catch (e) {}
 };
 
-const addNewTracks = async (sourcePlaylist: string, targetPlaylist: string) => {
-  const newTracks = await getTracks(sourcePlaylist);
-  const targetListTracks = (await getTracks(targetPlaylist)).map(
+const getNewTracks = async (playlistID: string) => {
+  const tracks = await getAllTracks(playlistID);
+
+  const newTracks = tracks
+    .filter(({ added_at }) => isLessThanAWeekOld(added_at))
+    .map((track) => track.track.uri);
+
+  const uniqueTracks = [...new Set(newTracks)];
+  return uniqueTracks;
+};
+
+export const addNewTracks = async (sourcePlaylist: string, targetPlaylist: string) => {
+  const newTracks = await getNewTracks(sourcePlaylist);
+  const targetListTracks = (await getAllTracks(targetPlaylist)).map(
     ({ track }) => track.uri
   );
 
-  const trackURIs = newTracks
-    .filter(({ added_at }) => isLessThanAWeekOld(added_at))
-    .map((track) => track.track.uri)
-    .filter((id) => !targetListTracks.includes(id));
+  const trackURIs = newTracks.filter((id) => !targetListTracks.includes(id));
 
   addTracks(targetPlaylist, trackURIs);
 };
