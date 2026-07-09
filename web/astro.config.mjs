fd79fe7b4e3692953e994340e6826d4cb4bc3851
Sophie Au <SophieAu@users.noreply.github.com>
2026-07-09 22:55:59 +0200

    Let's pray for env vars

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,6 +2,8 @@ import svelte from "@astrojs/svelte";
 import clerk from "@clerk/astro";
 import { defineConfig } from "astro/config";
 
+console.log("PUBLIC_CLERK_PUBLISHABLE_KEY visible to build:", Boolean(process.env.PUBLIC_CLERK_PUBLISHABLE_KEY));
+
 export default defineConfig({
   integrations: [clerk(), svelte()],
   vite: {
